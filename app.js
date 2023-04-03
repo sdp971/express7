280ab7f607eeba3a1ff8be022c3a1cc186861835
sdp971 <doris190485@hotmail.com>
2023-04-03 17:40:59 +0200

    done quest 08

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const {hashPassword}= require('./auth.js')
+const { hashPassword , verifyPassword, verifyToken }= require('./auth.js')
 
 const app = express();
 
@@ -16,20 +16,36 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require('./userHandlers');
 
+//the public routes
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
+app.get('/api/users', userHandlers.getUsers);
+app.get('/api/users/:id', userHandlers.getUserById);
+
+app.post('/api/users', hashPassword, userHandlers.postUser);
+app.post(
+  '/api/login',
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);// /!\ login should be a public route
+
+
+// then the routes to protect
+app.use(verifyToken)  // authentication wall : verifyToken is activated for each route after this line
+
+app.post("/api/movies",  movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/movies/:id",  movieHandlers.deleteMovie);
 
-const userHandlers = require("./userHandlers");
 
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id",hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
+
+
+
+
 
 app.listen(port, (err) => {
   if (err) {
@@ -38,3 +54,4 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
